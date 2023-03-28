91948d3f977a0d4d3ca992f47b47db4a2d8e15b4
naveenkurella8@gmail.com <naveenkurella8@gmail.com>
2023-03-28 23:31:32 +0530

    Day1.1

--- src/test/java/com/testninjalabs/qa/testcases/Login.java
+++ src/test/java/com/testninjalabs/qa/testcases/Login.java
@@ -9,8 +9,9 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Login {
+	
 
-	@Test
+	@Test(priority = 1)
 	public void verifyLoginWithValidCreds() {
 		
 		WebDriver driver=new ChromeDriver();
@@ -24,28 +25,25 @@ public class Login {
 		driver.findElement(By.cssSelector("input#input-password")).sendKeys("12345");
 		driver.findElement(By.cssSelector("input[value='Login']")).click();
 		Assert.assertTrue(driver.findElement(By.linkText("Edit your account information")).isDisplayed());
-		driver.close();
+		driver.quit();
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+	}
+	
+	@Test(priority=2)
+	public void verifyLoginWithInvalidCreds() {
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.get("http://tutorialsninja.com/demo/");
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.xpath("//a[text()=\"Login\"]")).click();
+		driver.findElement(By.xpath("//input[@name=\"email\"]")).sendKeys("nk@gmail.com2");
+		driver.findElement(By.cssSelector("input#input-password")).sendKeys("12345");
+		driver.findElement(By.cssSelector("input[value='Login']")).click();
+		String actual=driver.findElement(By.xpath("//div[contains(@class,'alert-dismissible')]")).getText();
+		Assert.assertTrue(actual.contains("Warning: No match for E-Mail Address and/or Password."));
+		driver.quit();	
 		
 	}
 }
