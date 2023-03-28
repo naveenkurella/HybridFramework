f2d61ec39b5ab4e55af7b4c7edc4d09302ace64d
naveenkurella8@gmail.com <naveenkurella8@gmail.com>
2023-03-28 22:33:02 +0530

    Day1

--- src/test/java/com/testninjalabs/qa/testcases/Login.java
+++ src/test/java/com/testninjalabs/qa/testcases/Login.java
@@ -1,5 +1,51 @@
 package com.testninjalabs.qa.testcases;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class Login {
 
+	@Test
+	public void verifyLoginWithValidCreds() {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.get("http://tutorialsninja.com/demo/");
+		driver.findElement(By.xpath("//span[text()='My Account']")).click();
+		driver.findElement(By.xpath("//a[text()=\"Login\"]")).click();
+		driver.findElement(By.xpath("//input[@name=\"email\"]")).sendKeys("nk@gmail.com");
+		driver.findElement(By.cssSelector("input#input-password")).sendKeys("12345");
+		driver.findElement(By.cssSelector("input[value='Login']")).click();
+		Assert.assertTrue(driver.findElement(By.linkText("Edit your account information")).isDisplayed());
+		driver.close();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 }
